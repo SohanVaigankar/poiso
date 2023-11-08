b4e8600850d04b205f2695184cac7bd633db3796
switch <switch@sohan.local>
2023-11-08 22:32:22 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// components
+import { Home } from "./pages";
+// images & icons
+import logo from "./assets/images/brand/poisologo.svg";
+// styles
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <img src={logo} alt="poiso" />
+
+      {/* <Home /> */}
+    </>
+  );
+};
+
+export default App;
